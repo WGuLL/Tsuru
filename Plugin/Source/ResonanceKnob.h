1409888f2b8909eea2ea16156cc0155f58cb3571
ROUSSEL Fabien <fabien.roussel.1992@gmail.com>
2020-08-09 16:56:01 +0200

    Displayed the resonance knob.

--- Plugin/Source/ResonanceKnob.h
+++ Plugin/Source/ResonanceKnob.h
@@ -1,9 +1,25 @@
 
+#include "JuceHeaderWrapper.h"
+#include "UiBroadcaster.h"
 /**
  * Allows the user to change the resonance
  * of the applied filter.
  */
-class ResonanceKnob
+class ResonanceKnob : public juce::Slider, public UiBroadcastedValueListener
 {
 public:
+    ResonanceKnob (UiBroadcaster& broadcaster_, std::function<void(double)> setResFunc) : juce::Slider (juce::Slider::RotaryVerticalDrag, juce::Slider::NoTextBox), broadcaster(broadcaster_)
+    {
+        setNormalisableRange ({0.01, 2.});
+        onValueChange (std::function<void()>([setResFunc, this](){setResFunc (getValue());
+        }));
+    }
+    
+     void valueChanged(double newValue) override
+    {
+        setValue (newValue, juce::dontSendNotification);
+    }
+    
+private:
+    UiBroadcaster& broadcaster;
 };
--- Plugin/Source/UiBroadcaster.h
+++ Plugin/Source/UiBroadcaster.h
@@ -11,6 +11,7 @@ using namespace std::chrono_literals;
 enum class ValueIds : size_t
 {
     filterCutoff,
+    filterResonance,
     nbValues
 };
 /**
